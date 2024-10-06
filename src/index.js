a405fefdb44c7e3761ab6d41d42cb64dafc13a88
KidSysco <kidsysco@gmail.com>
2024-10-05 22:00:22 -0500

    remove nftHelperStore template export

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-export * from './stores/nftHelperStore.js';
 export * from './stores/nftStore.js';
 export * from './composables/useEvmNft.js';
 export * from './composables/useEvmNftGallery.js';
 export * from './modules/blockchains.js';
 export * from './modules/dahDemoV1Abi.js';
 export * from './modules/dahNftV2Abi.js';
-// export { default as MyComponent } from './components/MyComponent.vue';
