c8fb24cbe5b14bd3224a5e51df47e1d324146c2a
KidSysco <kidsysco@gmail.com>
2025-03-01 00:18:37 -0600

    remove ABIs for TypeScript

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,12 +1,5 @@
-import { AbiItem } from './abi';
-
 export * from './useEvmNft';
 export * from './useEvmNftGallery';
 export * from './useEvmMetaDataGallery';
 export * from './useNftStore';
 export * from './blockchains';
-export * from './abi';
-
-export declare const dahDemoV1Abi: AbiItem[];
-export declare const dahNftV2Abi: AbiItem[];
-export declare const dahRev2omAbi: AbiItem[];
